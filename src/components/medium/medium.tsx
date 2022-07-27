9a95afa6a2dc8fc876ac2f8a7d66458ce89d4f94
Emanoel Barros <ebarros297@gmail.com>
2022-07-27 18:06:03 -0300

    feat: add medium structure

--- src/components/medium/medium.tsx
+++ src/components/medium/medium.tsx
@@ -7,8 +7,30 @@ type Props = {
 };
 
 function Medium({ data }: Props) {
-  const { name } = data;
-  return <div className={styles.container}>{name}</div>;
+  const { name, cover, status } = data;
+
+  const getStatusMessage = () => {
+    if (status === 'error') {
+      return 'Error in processing';
+    } else if (status === 'transcribing') {
+      return 'Transcribing';
+    } else {
+      // calculate edited time
+      return 'Edited';
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.cover}>
+        <img src={cover} alt="Cover" />
+      </div>
+      <div className={styles.details}>
+        <p className={styles.title}>{name}</p>
+        <p className={styles.status}>{getStatusMessage()}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Medium;

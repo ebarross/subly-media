1f0891b01c0b3bb5b3941ae266f492143f97c131
Emanoel Barros <ebarros297@gmail.com>
2022-07-27 21:51:06 -0300

    feat: add function to calculate last edited months

--- src/components/medium/medium.tsx
+++ src/components/medium/medium.tsx
@@ -1,27 +1,35 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { MediumData } from '../../interfaces/medium-data';
 import styles from './medium.module.css';
 import Alert from './alert/alert';
 import Backdrop from './backdrop/backdrop';
 import Actions from './actions/actions';
 
+const calcLastEditedInMonths = (edited: Date): number => {
+  const current = new Date();
+  let diff = (current.getFullYear() - edited.getFullYear()) * 12;
+  diff -= edited.getMonth() + 1;
+  diff += current.getMonth();
+  return diff <= 0 ? 0 : diff;
+};
+
 type Props = {
   data: MediumData;
 };
 
 function Medium({ data }: Props) {
-  const { name, cover, status, languages } = data;
+  const { name, cover, status, languages, updatedAt } = data;
 
-  const getStatusMessage = () => {
+  const statusMessage = useMemo(() => {
     if (status === 'error') {
       return 'Error in processing';
     } else if (status === 'transcribing') {
       return 'Transcribing';
     } else {
-      // calculate edited time
-      return 'Edited';
+      const months = calcLastEditedInMonths(new Date(updatedAt));
+      return `Edited ${months} months ago`;
     }
-  };
+  }, [status, updatedAt]);
 
   const renderCover = () => {
     if (status === 'error') {
@@ -45,7 +53,7 @@ function Medium({ data }: Props) {
       <div className={styles.cover}>{renderCover()}</div>
       <div className={styles.details}>
         <p className={styles.title}>{name}</p>
-        <p className={styles.status}>{getStatusMessage()}</p>
+        <p className={styles.status}>{statusMessage}</p>
       </div>
     </div>
   );

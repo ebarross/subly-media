145186a91bd985f16d49591d7b95405aca700292
Emanoel Barros <ebarros297@gmail.com>
2022-07-27 21:54:20 -0300

    refactor: adjust error alert component

--- src/components/medium/alert/alert.tsx
+++ src/components/medium/alert/alert.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Button } from '../../../components';
+import { ReactComponent as ExclamationIcon } from '../../../assets/exclamation.svg';
 import styles from './alert.module.css';
 
 function Alert() {
   return (
     <div className={styles.container}>
-      <div>
-        <p className={styles.message}>
+      <div className={styles.messageContainer}>
+        <span className={styles.icon}>
+          <ExclamationIcon fill="#fc2356" height={20} width={20} />
+        </span>
+        <span className={styles.message}>
           An error occurred while processing your file. Delete file to try
           again, and report issue if the problem persists.
-        </p>
+        </span>
       </div>
       <div className={styles.actions}>
         <Button variant="secondary">Delete file</Button>
